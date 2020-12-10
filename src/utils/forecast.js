49599c542bf67e880dcfbe41abb457005b761f80
Samir Muratovic <ssamirmuratovic@gmail.com>
2020-12-10 14:59:42 -0500

    add proxy redirect for mixed active content

--- src/utils/forecast.js
+++ src/utils/forecast.js
@@ -15,7 +15,7 @@ const getForecast = async (city) => {
     const longitude = data.features[0].center[0]
     const location = data.features[0].place_name
 
-    const forecastURL = `http://api.weatherstack.com/current?access_key=${process.env.REACT_APP_FORECAST_KEY}&query=${latitude},${longitude}&units=f`
+    const forecastURL = `https://cors-anywhere.herokuapp.com/http://api.weatherstack.com/current?access_key=${process.env.REACT_APP_FORECAST_KEY}&query=${latitude},${longitude}&units=f`
 
     res = await fetch(forecastURL)
     data = await res.json()
