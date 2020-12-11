3d862d7af4fab7781f459438515c81696a4bd832
Samir Muratovic <ssamirmuratovic@gmail.com>
2020-12-11 13:01:38 -0500

    try new build script

--- src/server.js
+++ src/server.js
@@ -4,7 +4,7 @@ const getForecast = require('./utils/forecast')
 
 // create express app
 const app = express()
-const port = process.env.PORT || 3000
+const port = process.env.PORT || 8080
 
 app.use(express.json())
 app.use(express.static(path.join(__dirname, 'build')))
